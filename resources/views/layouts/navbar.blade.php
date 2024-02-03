aa9ac339cfee88d5f2b49b29a19f7558fa6e396e
Fattan2005 <fattanhibrizi@gmail.com>
2024-02-03 20:23:03 +0700

    +footer & section 2

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -25,72 +25,97 @@
 <body style="background-color: rgb(246, 246, 246)" >
 
     {{-- navbar --}}
-    <nav class="navbar navbar-expand-md bg-white" style="font-family: manrope; font-weight: 600" >
-         <div class="container pb-2 " {{--style="background-color: #FFF60F;" --}} >
-
-            <div class=" col-lg-3 d-flex" {{--style="background-color: blue"--}}>
-                <a class="navbar-brand d-flex align-item-center" href="">
-                    <img src="{{asset('assets/logo.png')}}" style="width:3.2rem; height: 3rem;" alt="">
-                    <p class="pt-2  px-2 fs-6">Cuan Aggregators</p>
-                </a>
-            </div>
-
-            <button class="navbar-toggler flex-row-reverse" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                {{-- <span class="navbar-toggler-icon"></span> --}}
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#FFF60F" class="bi bi-list" viewBox="1 1 14 14" style="border-widht: 0; border-color: white">
-                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-                </svg>
-            </button>
-
-            <div class="col-12 col-lg-9 " style=" font-size: 2vh">
-                <div class="collapse navbar-collapse flex-row-reverse gap-3" id="navbarNavDropdown">
-                    <ul class="navbar-nav text-center ">
-
-                        @if(Route::is('home')) 
-                        <li class="nav-item mb-0">
-                            <a class="nav-link active" aria-current="page" href="{{route('home')}}">Beranda</a>
-                            <hr style="margin-top: -0.5vh; size: 40px">
-                        </li>
-                        @else
-                        <li class="nav-item mb-0">
-                            <a class="nav-link" aria-current="page" href="{{route('home')}}">Beranda</a>
-                        </li>
-                        @endif
-
-                        
-                        @if(Route::is('simulasikpr')) 
-                        <li class="nav-item mb-0">
-                            <a class="nav-link active" aria-current="page" href="{{route('simulasikpr')}}">Simulasi Perhitungan KPR</a>
-                            <hr style="margin-top: -0.5vh; size: 40px">
-                        </li>
-                        @else
-                        <li class="nav-item mb-0">
-                            <a class="nav-link" aria-current="page" href="{{route('simulasikpr')}}">Simulasi Perhitungan KPR</a>
-                        </li>
-                        @endif
-
-                        @if(Route::is('tentangkami')) 
-                        <li class="nav-item mb-0">
-                            <a class="nav-link active" aria-current="page" href="{{route('tentangkami')}}">Tentang Kami</a>
-                            <hr style="margin-top: -0.5vh; size: 40px">
-                        </li>
-                        @else
-                        <li class="nav-item mb-0">
-                            <a class="nav-link" aria-current="page" href="{{route('tentangkami')}}">Tentang Kami</a>
-                        </li>
-                        @endif
-                        
-                    </ul>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-white fixed-top" style="font-family: manrope; font-weight: 600;" >
+             <div class="container pb-2 " {{--style="background-color: #FFF60F;" --}} >
+    
+                <div class=" col-lg-3 d-flex" {{--style="background-color: blue"--}}>
+                    <a class="navbar-brand d-flex align-item-center" href="">
+                        <img src="{{asset('assets/logo.png')}}" style="max-width:3.2rem; max-height: 3rem;" alt="">
+                        {{-- <p class="pt-2  px-2" style="font-size: 1rem">Cuan Aggregators</p> --}}
+                    </a>
                 </div>
+    
+                <button class="navbar-toggler flex-row-reverse" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    {{-- <span class="navbar-toggler-icon"></span> --}}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#FFF60F" class="bi bi-list" viewBox="1 1 14 14" style="border-widht: 0; border-color: white">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+                </button>
+    
+                <div class="col-12 col-lg-9 " style=" font-size: 2vh">
+                    <div class="collapse navbar-collapse flex-row-reverse gap-3" id="navbarNavDropdown">
+                        <ul class="navbar-nav text-center ">
+    
+                            @if(Route::is('home')) 
+                            <li class="nav-item mb-0">
+                                <a class="nav-link active" aria-current="page" href="{{route('home')}}">Beranda</a>
+                                <hr style="margin-top: -1vh; height: 12px; "/>
+                            </li>
+                            @else
+                            <li class="nav-item mb-0">
+                                <a class="nav-link" aria-current="page" href="{{route('home')}}">Beranda</a>
+                            </li>
+                            @endif
+    
+                            
+                            @if(Route::is('simulasikpr')) 
+                            <li class="nav-item mb-0">
+                                <a class="nav-link active" aria-current="page" href="{{route('simulasikpr')}}">Simulasi Perhitungan KPR</a>
+                                <hr style="margin-top: -1vh; size: 12px"/>
+                            </li>
+                            @else
+                            <li class="nav-item mb-0">
+                                <a class="nav-link" aria-current="page" href="{{route('simulasikpr')}}">Simulasi Perhitungan KPR</a>
+                            </li>
+                            @endif
+    
+                            @if(Route::is('tentangkami')) 
+                            <li class="nav-item mb-0">
+                                <a class="nav-link active" aria-current="page" href="{{route('tentangkami')}}">Tentang Kami</a>
+                                <hr style="margin-top: -1vh; size: 12px;"/>
+                            </li>
+                            @else
+                            <li class="nav-item mb-0">
+                                <a class="nav-link" aria-current="page" href="{{route('tentangkami')}}">Tentang Kami</a>
+                            </li>
+                            @endif
+                            
+                        </ul>
+                    </div>
+                </div>
+    
             </div>
-
-        </div>
-    </nav>
+        </nav>
+    </header>
     
     {{-- content --}}
-    <div class="container mt-5">
+    <div class="container" style="margin-top: 8rem">
         @yield('content')
     </div>
+
+    {{-- footer --}}
+    <footer class=" bg-secondary">
+        <div class="mt-5 pt-5 container">
+            <div class=" text-white justify-content-md-evenly row">
+                <div class="hubungi-kami col-12 col-md-6 col-lg-4 mb-3">
+                    <h6>Hubungi Kami</h6>
+
+                </div>
+                <div class="tentang-kami col-12 col-md-6 col-lg-3 mb-3">
+                    <h6>Tentang Kami</h6>
+                </div>
+                <div class="media-sosial col-12 col-md-6 col-lg-4">
+                    <h6>Media Sosial</h6>
+
+                    <p>2024 Cuan Aggregators | Kebijakan Privasi</p>
+                </div>
+                <div class="logo col-12 col-md-6 col-lg-1">
+                    <img src="{{asset('assets/logo.png')}}" style="max-width:3.2rem; max-height: 3rem;" alt="">
+                </div>
+            </div>
+        </div>
+    </footer>
 
     {{-- script --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -6,19 +6,176 @@
 
 @section('content')
 
-    {{-- section 1 --}}
-<div class="col-12 d-flex  justify-content-between">
+{{-- section 1 --}}
+<div class="col-12 d-lg-flex justify-content-md-between" style="margin-bottom: 4rem">
 
-
-    <div class="col-6" style="font-family: poppins">
-        <h1 style="font-size: 3.2vmax; font-weight: bold" class="mb-4">Penuhi Kebutuhan Finansial Anda Bersama Kami</h1>
+    <div class="col-12 col-lg-6  justify-content-center mb-5" style="font-family: poppins">
+        <h1 style="font-size: 3.2rem; font-weight: bold" class="mb-4">Penuhi Kebutuhan Finansial Anda Bersama Kami</h1>
         <p style="font-size: 1vmax;">Cuan Aggregators by Loan Adviser kami siap membantu memenuhi kebutuhan finansial Anda dalam bentuk kredit, modal usaha, deposito, investasi, hingga take over. Konsultasikan dengan kami untuk mendapatkan pelayanan terbaik</p>
         <a target=”_blank” class="btn btn-primary" href="https://api.whatsapp.com/send?text=Hai%2C+Saya+tertarik+dengan+promo+KPR+di+banner+ini.&phone=6285210845762">Pelajari Lebih Lanjut</a>
     </div>
 
-    <div class="col-5 ">
-        <img class="mx-auto px-1"src="{{asset('assets/Home.png')}}" alt="" style="border-radius: 40% 0 40% 0; width: 35vw" >
+    <div class="col-12 col-lg-5">
+        <img class="mx-auto px-1"src="{{asset('assets/Home.png')}}" alt="" style="border-radius: 45% 45% 45% 0; width: 50ch;" >
     </div>
 
+</div>
+
+{{-- <div class="container container-product">
+    <div class="card card-product">
+        <div class="grid justify-content-center product-desktop">
+            <div class="g-col-2">
+                <a href="https://www.loanmarket.co.id/product/kpr-multiguna?product=kpr-primary" id="product-kpr-primary" class="text-center">
+                    <div class="card boxproduct">
+                        <img src="assets/img/icon/icon-kredit-rumah.png" class="card-img-top text-center" alt="...">
+                        <h5 class="card-title card-title-sm t-1">KPR dan Multiguna</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-2">
+                <a href="https://www.loanmarket.co.id/product/take-over?product=take-over" id="product-teake-over" class="text=center">
+                    <div class="card boxproduct  ">
+                        <img src="assets/img/icon/icon-take-over.png" alt="...">
+                        <h5 class="card-title card-title-sm t-1">Take Over <br>dan Bridging Loan</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-2">
+                <a href="https://www.loanmarket.co.id/product/refinancing?product=refinancing-kendaraan-bermotor" id="product-refinancing" class="text=center">
+                    <div class="card boxproduct  ">
+                        <img src="assets/img/icon/icon-gadai.png" alt="...">
+                        <h5 class="card-title card-title-sm t-1">Refinancing</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-2">
+                <a href="https://www.loanmarket.co.id/product/kredit-modal-usaha?product=kredit-modal-kerja" id="product-kredit-modal" class="text=center">
+                    <div class="card boxproduct  ">
+                        <img src="assets/img/icon/icon-modal-usaha.png" alt="...">
+                        <h5 class="card-title card-title-sm t-1">Kredit Modal Usaha<br> dan Investasi</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-2">
+                <a href="https://www.loanmarket.co.id/product/deposito?product=deposito-berjangka" id="product-deposito" class="text=center">
+                    <div class="card boxproduct  ">
+                        <img src="assets/img/icon/icon-deposito.png" alt="...">
+                        <h5 class="card-title card-title-sm t-1">Deposito</h5>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="grid justify-content-center product-mobile carousel slide" id="product-mobile">
+            <div class="carousel-inner container pt-2" style="height: 120px">
+                <div class="carousel-item active" style="height: 120px">
+                    <div class="row" style="margin-left: 0; margin-right: 0">
+                        <div class="col-6 pr-2">
+                            <a href="https://www.loanmarket.co.id/product/kpr-multiguna?product=kpr-primary" id="product-kpr-primary" class="text-center">
+                                <div class="card boxproduct float-right">
+                                    <img src="assets/img/icon/icon-kredit-rumah.png" class="card-img-top text-center" alt="...">
+                                    <h5 class="card-title card-title-sm t-1">KPR dan Multiguna</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-6 pr-2">
+                            <a href="https://www.loanmarket.co.id/product/take-over?product=take-over" id="product-teake-over" class="text=center">
+                                <div class="card boxproduct float-left">
+                                    <img src="assets/img/icon/icon-take-over.png" alt="...">
+                                    <h5 class="card-title card-title-sm t-1">Take Over <br>dan Bridging Loan</h5>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item" style="height: 120px">
+                    <div class="row" style="margin-left: 0; margin-right: 0">
+                        <div class="col-6 pl-2">
+                            <a href="https://www.loanmarket.co.id/product/refinancing?product=refinancing-kendaraan-bermotor" id="product-refinancing" class="text=center">
+                                <div class="card boxproduct float-right">
+                                    <img src="assets/img/icon/icon-gadai.png" alt="...">
+                                    <h5 class="card-title card-title-sm t-1">Refinancing</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-6 pr-2">
+                            <a href="https://www.loanmarket.co.id/product/kredit-modal-usaha?product=kredit-modal-kerja" id="product-kredit-modal" class="text=center">
+                                <div class="card boxproduct float-left">
+                                    <img src="assets/img/icon/icon-modal-usaha.png" alt="...">
+                                    <h5 class="card-title card-title-sm t-1">Kredit Modal Usaha<br> dan Investasi</h5>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item" style="height: 120px">
+                    <div class="row" style="margin-left: 0; margin-right: 0">
+                        <div class="col-6 pl-2">
+                            <a href="https://www.loanmarket.co.id/product/deposito?product=deposito-berjangka" id="product-deposito" class="text=center">
+                                <div class="card boxproduct float-right">
+                                    <img src="assets/img/icon/icon-deposito.png" alt="...">
+                                    <h5 class="card-title card-title-sm t-1">Deposito</h5>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev pt-2" href="#product-mobile" role="button" data-slide="prev" style="height: 100px">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next pt-2" href="#product-mobile" role="button" data-slide="next" style="height: 100px">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+        </div>
+    </div>
+</div> --}}
+
+
+
+{{-- section 2 --}}
+
+<div class="col-12">
+    <h1 >test test</h1>
+</div>
+
+{{-- section 3 --}}
+
+<div class="col-12" style="margin-top: 8rem">
+
+    <div class="row justify-content-around">
+        <div class=" col-12 text-center mb-4" style="font-family: poppins">
+            <h2 style=" font-weight: bold">Cara Mengajukan KPR di Cuan Aggregators</h2>
+        </div>
+        <div class="col-6 col-lg-3 text-center" >
+            <img src="{{asset('assets/Step.png')}}" class="card-img-center p-1 rounded-4 img-fluid mb-3" alt="Memilih Produk" >
+            <div class="card-body">
+                <h6>Memilih Produk</h6>
+                <p class="card-text">Pilih salah satu produk finansial yang Anda inginkan disini</p>
+            </div>
+        </div>
+        <div class=" col-6 col-lg-3 text-center" >
+            <img src="{{asset('assets/Step2.png')}}" class="card-img-center p-1 rounded-4 img-fluid mb-3" alt="Konsultasi Dengan Kami photo" >
+            <div class="card-body">
+                <h6>Konsultasi Dengan Kami</h6>
+                <p class="card-text">Anda akan dihubungi oleh Loan Adviser kami untuk berkonsultasi mengenai langkah kedepannya</p>
+            </div>
+        </div>
+        <div class=" col-6 col-lg-3 text-center" >
+            <img src="{{asset('assets/Step3.png')}}" class="card-img-center p-1 rounded-4 img-fluid mb-3" alt="Mengisi Data photo" >
+            <div class="card-body">
+                <h6>Mengisi Data</h6>
+                <p class="card-text">SLengkapi data dan dokumen pendukung yang diperlukan sesuai arahan Loan Adviser kami</p>
+            </div>
+        </div>
+        <div class=" col-6 col-lg-3 text-center" >
+            <img src="{{asset('assets/Step4.png')}}" class="card-img-center p-1 rounded-4 img-fluid mb-3" alt="Track Progress photo" >
+            <div class="card-body">
+                <h6>Track Progress</h6>
+                <p class="card-text">Jika sudah lengkap, Anda tinggal menunggu permintaan Anda disetujui dan siap digunakan</p>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
