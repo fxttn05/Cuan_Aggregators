32672506e8f8dd2341ff264f51d6f1d04b3c4c5b
Fattan2005 <fattanhibrizi@gmail.com>
2024-02-03 23:06:20 +0700

    tentang kami section 1

--- resources/views/web/tentangKami.blade.php
+++ resources/views/web/tentangKami.blade.php
@@ -5,5 +5,15 @@
 @endsection
 
 @section('content')
-<h1>test 2</h1>
+{{-- section 1 --}}
+<div class="col-12 row gap-5">
+    <h1>Tentang Kami</h1>
+    <div class="col-12 col-lg">
+        <p class="">Dengan menggunakan jasa kami, para nasabah telah berhasil membeli rumah pertama maupun kedua mereka dengan penawaran harga dan suku bunga terbaik KPR bank rekanan kami. Begitupun untuk nasabah yang melakukan penghematan cicilan KPR dengan melakukan Take Over KPR tentunya dengan suku bunga terbaik dari bank rekanan kami.<br><br>Oleh karena itu, dengan data kolektif, Mortgage Master telah membantu nasabah dalam menghemat KPR maupun cicilan KPR dengan Take Over KPR senilai lebih dari ratusan miliar.<br><br>Bersama kami, bukan hanya hemat dari segi biaya melainkan hemat waktu karena Anda tidak perlu mendatangi beberapa bank karena kita memiliki berbagai produk dan pilihan bank yang tentunya sesuai dengan kebutuhan Anda.</p>
+    </div>
+    <div class="col-12 col-lg">
+        <img src="{{asset('assets/tentang-kami.png')}}" style="width: 30rem" alt="" class="rounded-4 ms-lg-5">
+    </div>
+</div>
+
 @endsection
